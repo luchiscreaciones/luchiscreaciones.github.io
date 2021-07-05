197019031dc8ed99c0cb4b420c171cbe8c55cf1c
Brian Alvarez <brianalvarez2000@gmail.com>
2021-07-05 15:36:35 -0500

    LuchiCreaciones

---
+++ correo.php
@@ -0,0 +1,18 @@
+<?php
+    $destinatario = 'brianalvarez2000@gmail.com';
+
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $mensaje = $_POST['mensaje'];
+    $asunto = "Estoy interesado en los cursos";
+
+    $header = "Inscripcion desde la pagina Web";
+    $mensajeCompleto = $mensaje . "\nAtentamente" . $nombre;
+
+    mail($destinatario, $asunto, $mensajeCompleto, $header);
+
+    echo "<script>alert('Inscripcion realizada exitosamente')</script>";
+    echo "<script>setTimeout(\"location.href='index.html'\",1000)</script>";
+
+
+?>
